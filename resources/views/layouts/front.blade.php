238d4004b361d7e23f52e46efedf2ce7d83339c9
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-07-22 06:13:28 +0200

    Fix/suppress-extra-margins

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -2,8 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Gospel Dream')</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alata&amp;display=swap">
@@ -17,43 +16,45 @@
     {{ $scripts ?? null }}
 </head>
 <body>
-    <div class="main-wrapper mt-4 mx-auto">
+    <div class="main-wrapper mt-4">
         @include('front.header-nav')
         <div class="o-section-title dd-flex p-5">
             <h1 class="a-heading a-heading-1">{{ $h1 }}</h1>
         </div>
         <div id="main" class="o-main-content">
-            <div class="d-flex flex-column flex-lg-row main-container p-4">
-                <div class="main-column p-4">
-                {{ $slot }}
-                </div>
+            <div class="main-container d-flex flex-column flex-lg-row  p-4">
+                <div class="main-column p-4">{{ $slot }}</div>
                 <div class="aside-column bg-body">
                     <div id="contact-block-head">
-                        <p class="fs-4 mb-1 p-2 tex text-center text-white">Réservations</p>
+                        <p class="fs-4 mb-1 p-2 tex text-center">Réservations</p>
                     </div>
-                    <p class="o-contact-block w-100 p-2">
-                        <span class="d-flex w-100 a-heading"> <a href="http://www.fnactickets.com/recherche/rechercheRapide.do?search=gospel+dream" class="border-0 btn btn-outline-dark btn-primary p-2 text-white w-100">Acheter sur la Fnac</a> </span>
-                        <span class="d-flex w-100 mt-2"> <a href="http://www.francebillet.com/recherche/rechercheRapide.do?search=GOSPEL+DREAM" class="border-0 btn btn-outline-dark btn-primary p-2 text-white w-100">Acheter sur FranceBillet</a> </span>
+                    <p class="mt0 o-contact-block p-2 w-100">
+                        <span class="d-flex w-100 a-heading">
+                            <a href="http://www.fnactickets.com/recherche/rechercheRapide.do?search=gospel+dream" class="btn p-2 w-100">Acheter sur la Fnac</a>
+                        </span>
+                        <span class="d-flex w-100 mt-2">
+                            <a href="http://www.francebillet.com/recherche/rechercheRapide.do?search=GOSPEL+DREAM" class="btn p-2 w-100">Acheter sur FranceBillet</a>
+                        </span>
                     </p>
                     <div id="image-block">
                         <picture class="d-flex justify-content-center">
                             <img class="w-75" src="{{ asset('img/affiche.jpg') }}">
                         </picture>
                     </div>
-                    <p class="d-flex flex-column p-2">
-                        <span class="border-0 btn btn-outline-dark btn-primary p-2 text-white w-100 ">Tél. : 01 43 14 08 10</span>
-                        <span class="border-0 btn btn-outline-dark btn-primary p-2 text-white w-100 mt-2">Port. : 06 07 08 55 56</span>
+                    <p class="d-flex flex-column mb-0 p-2">
+                        <span class="btn p-2 w-100">Tél. : 01 43 14 08 10</span>
+                        <span class="btn p-2 w-100 mt-2">Port. : 06 07 08 55 56</span>
                     </p>
                     <div id="video-block">
-                        <p class="fs-4 mb-0 p-2 tex text-center text-white fs-5">Presse Pack</p>
-                        <iframe src="//www.youtube.com/embed/C8I3TzXBMA4" allowfullscreen="" frameborder="0" height="143" width="100%"></iframe>
-                </div>
+                        <p class="fs-4 mb-0 p-2 tex text-center fs-5">Presse Pack</p>
+                        <iframe src="//www.youtube.com/embed/C8I3TzXBMA4" frameborder="0" height="143" width="100%"></iframe>
+                    </div>
                 </div>
             </div>
         </div>
         <footer class="d-flex flex-column flex-lg-row p-3">
             <div class="footer-copyrights text-center d-flex align-items-center">
-                <p class="mb-0 text-center"> Tous droits réservés www.gospeldream.com ©2022 </p>
+                <p class="mb-0 text-center">Tous droits réservés www.gospeldream.com ©2022</p>
             </div>
             <div class="footer-mentions text-center d-flex align-items-center ms-2">
                 <a href="{{ url('mentions-legales') }}" class="text-decoration-none text-primary">Consultez nos mentions légales</a>
@@ -71,10 +72,10 @@
             </div>
         </footer>
     </div>
-<script src="{{ asset('js/jquery.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.js"></script>
-<script src="{{ asset('js/Lightbox-Gallery.js') }}"></script>
-<script src="{{ asset('js/scripts.js') }}"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.js"></script>
+    <script src="{{ asset('js/Lightbox-Gallery.js') }}"></script>
+    <script src="{{ asset('js/scripts.js') }}"></script>
 </body>
 </html>
